82ff10c1903b87003ceccbe58db952b6459af972
guochongshen <434336389@qq.com>
2022-04-01 21:32:33 +0800

    collect/themeColor update

--- pages/order/index.js
+++ pages/order/index.js
@@ -1,66 +1,64 @@
 // pages/order/index.js
+import { request } from "../../request/index.js";
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        tabs: [{
+                id: 0,
+                value: "综合",
+                isActive: true
+            },
+            {
+                id: 1,
+                value: "销量",
+                isActive: false
+            },
+            {
+                id: 2,
+                value: "价格",
+                isActive: false
+            },
+            {
+                id: 3,
+                value: "退款/退货",
+                isActive: false
+            }
+        ]
     },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
+    onShow(option) {
+        const token = wx.getStorageSync("token");
+        if (!token) {
+            wx.navigateTo({
+                url: '/pages/auth/index'
+            });
+            return;
+        }
+        // 1 获取当前的小程序的页面栈-数组 长度最大是10页面
+        let pages = getCurrentPages();
+        // 2 数组中 索引最大的页面就是当前页面
+        let currentPage = pages[pages.length - 1];
+        // 3 获取url上的type参数
+        const { type } = currentPage.options;
+        this.getOrders(type);
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
+    // 获取订单列表的方法
+    async getOrders(type) {
+        const res = await request({ url: "/my/orders/all", data: { type } });
+        console.log(res);
     },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    handleTabsItemChange(e) {
+        // 1 获取被点击的标题索引
+        const { index } = e.detail
+            // 2 修改原数组
+        let { tabs } = this.data;
+        tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
+        // 3 赋值到data中
+        this.setData({
+            tabs
+        })
     }
 })
